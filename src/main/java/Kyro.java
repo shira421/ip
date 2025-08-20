d28fbe50a07674a98843dd88afa980c4bcdbd70c
Ksheerabthi Nathan <vksheerabthi@gmail.com>
2025-08-20 19:42:27 +0800

    Level-0

---
+++ src/main/java/Kyro.java
@@ -0,0 +1,23 @@
+public class Kyro {
+    public static void main(String[] args) {
+        String logo =
+                " ____  __.                      \n"
+                        + "|    |/ _|___.__._______  ____  \n"
+                        + "|      < <   |  |\\_  __ \\/  _ \\ \n"
+                        + "|    |  \\ \\___  | |  | \\(  <_> )\n"
+                        + "|____|__ \\/ ____| |__|   \\____/ \n"
+                        + "        \\/\\/                     \n";
+
+        String LINE = "____________________________________________________________";
+
+        System.out.println("Hello from\n" + logo);
+
+        System.out.println(LINE);
+        System.out.println(" Hello! I'm Kyro, your personal assistant");
+        System.out.println(" What can I do for you?");
+        System.out.println(LINE);
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+}
