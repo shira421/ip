fe77b27a50495380aa94268aa5414856fe4e2aab
Ksheerabthi Nathan <vksheerabthi@gmail.com>
2025-08-27 11:56:18 +0800

    added list function, minor edits to kyro's print messages

--- src/main/java/Kyro.java
+++ src/main/java/Kyro.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class Kyro {
 
     private static final String LINE = "____________________________________________________________";
+    private static final int MAX_TASKS = 100;
 
-    public static void main(String[] args) {
+    public static void printWelcome() {
         String logo = " ____  __.                      \n"
                 + "|    |/ _|___.__._______  ____  \n"
                 + "|      < <   |  |\\_  __ \\/  _ \\ \n"
@@ -16,26 +17,48 @@ public class Kyro {
 
         System.out.println(LINE);
         System.out.println(" Hello! I'm Kyro, your personal assistant");
-        System.out.println(" What can I do for you?");
+        System.out.println(" What can Kyro do for you?");
         System.out.println(LINE);
+    }
 
+    public static void printBye() {
+        System.out.println(LINE);
+        System.out.println(" Bye. Kyro hopes to see you again soon!");
+        System.out.println(LINE);
+    }
+
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
+
+        printWelcome();
 
         while (true) {
-            String userInput = scanner.nextLine();
+            String input = scanner.nextLine().trim();
 
-            if (userInput.equals("bye")) {
-                System.out.println(LINE);
-                System.out.println(" Bye. Hope to see you again soon!");
-                System.out.println(LINE);
+            if (input.equalsIgnoreCase("bye")) {
+                printBye();
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println(LINE);
+                if (taskCount == 0) {
+                    System.out.println(" Kyro is glad that you have no tasks!");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.printf(" %d. %s%n", i + 1, tasks[i]);
+                    }
+                }
+                System.out.println(LINE);
+            } else if (input.isEmpty()) {
+                continue;
+            } else {
+                tasks[taskCount++] = input;
+                System.out.println(LINE);
+                System.out.println(" added: " + input);
+                System.out.println(LINE);
             }
-
-            System.out.println(LINE);
-            System.out.println(" " + userInput);
-            System.out.println(LINE);
         }
-
         scanner.close();
     }
 }
