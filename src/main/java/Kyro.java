d5da99925e4e0a985f165d7b61f840e59c5e85be
Ksheerabthi Nathan <vksheerabthi@gmail.com>
2025-08-27 11:29:58 +0800

    added echo user input

--- src/main/java/Kyro.java
+++ src/main/java/Kyro.java
@@ -1,14 +1,16 @@
-public class Kyro {
-    public static void main(String[] args) {
-        String logo =
-                " ____  __.                      \n"
-                        + "|    |/ _|___.__._______  ____  \n"
-                        + "|      < <   |  |\\_  __ \\/  _ \\ \n"
-                        + "|    |  \\ \\___  | |  | \\(  <_> )\n"
-                        + "|____|__ \\/ ____| |__|   \\____/ \n"
-                        + "        \\/\\/                     \n";
+import java.util.Scanner;
 
-        String LINE = "____________________________________________________________";
+public class Kyro {
+
+    private static final String LINE = "____________________________________________________________";
+
+    public static void main(String[] args) {
+        String logo = " ____  __.                      \n"
+                + "|    |/ _|___.__._______  ____  \n"
+                + "|      < <   |  |\\_  __ \\/  _ \\ \n"
+                + "|    |  \\ \\___  | |  | \\(  <_> )\n"
+                + "|____|__ \\/ ____| |__|   \\____/ \n"
+                + "        \\/\\/                     \n";
 
         System.out.println("Hello from\n" + logo);
 
@@ -17,7 +19,23 @@ public class Kyro {
         System.out.println(" What can I do for you?");
         System.out.println(LINE);
 
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(LINE);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("bye")) {
+                System.out.println(LINE);
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(LINE);
+                break;
+            }
+
+            System.out.println(LINE);
+            System.out.println(" " + userInput);
+            System.out.println(LINE);
+        }
+
+        scanner.close();
     }
 }
