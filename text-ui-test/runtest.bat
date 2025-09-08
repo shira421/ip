91e832e95db60adf2c76ffcc917a0cbdbb0f6ff1
Ksheerabthi Nathan <vksheerabthi@gmail.com>
2025-09-09 02:35:59 +0800

    minor edits to packages and readme

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -6,16 +6,15 @@ if not exist ..\bin mkdir ..\bin
 REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
-REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+REM compile all Java files from src/main/java (including subfolders) into bin
+javac -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\kyro\*.java ..\src\main\java\kyro\exceptions\*.java ..\src\main\java\kyro\tasks\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
 )
-REM no error here, errorlevel == 0
 
-REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ..\bin Kyro < input.txt > ACTUAL.TXT
+REM run the program using fully qualified class name (package kyro)
+java -classpath ..\bin kyro.Kyro < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output, ignoring line ending differences
 fc /w ACTUAL.TXT EXPECTED.TXT > NUL
